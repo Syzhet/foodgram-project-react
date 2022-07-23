f9a7dfd66b257892b38572b3a3bd5a2c3588a736
Алексей Ионов <click742@yandex.ru>
2022-07-23 21:18:28 +0300

    Add model Recipe, add MEDIA_ROOT and MEDIA_URL, add url for MEDIA if DEBUG=TRUE

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -137,3 +137,7 @@ DJOSER = {
         'user_list': ['rest_framework.permissions.AllowAny']
     },
 }
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.core import validators
+
+from users.models import CustomUser
+
+
+MIN_TIME_LIMIT = 'Время приготовления не может быть меньше 1-й минуты'
+VALUE_MIN_TIME_LIMIT = 1
 
 
 class Tag(models.Model):
@@ -10,6 +17,9 @@ class Tag(models.Model):
         ordering = ['name']
         verbose_name = 'Тэг'
         verbose_name_plural = 'Тэги'
+
+    def __str__(self):
+        return self.name
 
 
 class Ingredient(models.Model):
@@ -24,3 +34,44 @@ class Ingredient(models.Model):
             models.UniqueConstraint(fields=['name', 'measurement_unit'],
                                     name='unique_ingridient_and_measurement')
         ]
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    tags = models.ManyToManyField(
+        Tag,
+        through='TagRecipe',
+        related_name='tags_for_recipe',
+        verbose_name='Тэги'
+    )
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор рецепта'
+    )
+    ingridients = models.ManyToManyField(
+        Ingredient,
+        through='IngredientRecipe',
+        related_name='ingridients_for_recipe',
+        verbose_name='Ингредиенты'
+    )
+    name = models.CharField('Название рецепта', max_length=200)
+    image = models.ImageField(
+        'Изображение бллюда',
+        upload_to='recipes/%Y/%m/%d/'
+    )
+    text = models.TextField('Описание рецепта')
+    cooking_time = models.PositiveSmallIntegerField(
+        'Время приготовления блюда',
+        validators=[validators.MinValueValidator(
+            limit_value=VALUE_MIN_TIME_LIMIT,
+            message=MIN_TIME_LIMIT
+            )
+        ]
+    )
+
+
+# Дописать связные таблицы для Тэгов и для Ингредиентов
