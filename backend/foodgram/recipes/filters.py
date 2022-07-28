afcc7169657c322fa702d79541aee02ddd1dc0eb
Алексей Ионов <click742@yandex.ru>
2022-07-28 17:30:33 +0300

    Fix bug with fiters.py in recipe in is_in_shopping_cart

--- backend/foodgram/recipes/filters.py
+++ backend/foodgram/recipes/filters.py
@@ -27,5 +27,5 @@ class RecipeFilter(FilterSet):
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if value:
-            return queryset.filter(carts__user=self.request.user)
+            return queryset.filter(shoppinglist__user=self.request.user)
         return queryset
